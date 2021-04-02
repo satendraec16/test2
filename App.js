42aa7af3038c85411f4ac7bae39feedcc606cbe4
satendra kumar <satendraec16@gmail.com>
2021-04-02 12:20:35 +0530

    upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View ,Text, StyleSheet,Image} from 'react-native';
+
+const App=()=>{
+  return(
+    <Image 
+    source={require('./assets/Image/Ellipse 111.png')}
+    style={{width:200,height:200}}
+
+    />
+  );
+};
+
+export default App;
